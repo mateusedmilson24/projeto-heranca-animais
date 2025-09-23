a241c35fa9b3f52c068edfd09350f377b192152f
mateusedmilson24 <mateusrecruta2021@gmail.com>
2025-09-23 11:25:47 -0300

    Animal.java

--- src/main/java/br/edu/seuprojeto/modelo/Animal.java
+++ src/main/java/br/edu/seuprojeto/modelo/Animal.java
@@ -1,32 +1,31 @@
-package br.edu.seuprojeto.modelo;
-public class Animal {
-private String nome;
-private int idade;
+ package br.edu.seuprojeto.modelo;
 
-public Animal(String nome, int idade) {
-this.nome = nome;
-this.idade = idade;
-}
-public String getNome() {
-return nome;
-}
- public void setNome(String nome) {
-this.nome = nome;
-}
-public int getIdade() {
-return idade;
-}
-public void setIdade(int idade) {
-this.idade = idade;
-}
-public String emitirsom() {
-return "Som genérico de animal...";
-}
-@Override
-public String toString(){
-return"Animal{"+
-"nome='" + nome + '\''+
-", idade =" + idade +
-'} ';
-}
+public abstract class Animal {
+    protected String nome;
+    protected int idade;
+
+    public Animal(String nome, int idade) {
+        this.nome = nome;
+        this.idade = idade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void comer() {
+        System.out.println(this.nome + " está comendo.");
+    }
+
+    // Este é o método abstrato que as subclasses devem implementar
+    public abstract void emitirSom();
+
+    @Override
+    public String toString() {
+        return "Animal [nome=" + nome + ", idade=" + idade + "]";
+    }
 }
